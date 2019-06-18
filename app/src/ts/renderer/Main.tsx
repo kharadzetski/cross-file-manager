5e023bd87a6f0e04833efeb704b654a57be9ead3
Gorodetski Konstantin <kgorodetsky@yandex.ru>
2019-06-18 12:15:19 +0300

    First demo loading dir

--- app/src/ts/renderer/Main.tsx
+++ app/src/ts/renderer/Main.tsx
@@ -1,14 +1,26 @@
-import * as React from 'react';
+import React from 'react';
+import fs from 'fs';
+import os from 'os';
 
 import { increment } from '@renderer/utils/increment';
 
-export interface HelloProps { compiler: string; framework: string;
+export interface HelloProps { 
+  compiler: string;
+  framework: string;
 }
 
-export class Main extends React.Component<HelloProps, { counter: number }> {
+interface MainState {
+  files: string[];
+  counter: number;
+  prevDir?: string;
+  currentDir: string;
+}
+
+export class Main extends React.Component<HelloProps, MainState> {
   constructor(props: HelloProps) {
     super(props);
-    this.state = {counter: 0};
+    this.state = {counter: 0, files: [], currentDir: os.homedir()};
+    this.loadDir(this.state.currentDir);
   }
 
   private btnClick = (): boolean => {
@@ -16,7 +28,25 @@ export class Main extends React.Component<HelloProps, { counter: number }> {
     return false;
   };
 
+  private loadPrevDir = () => {
+    this.loadDir(this.state.prevDir);
+  }
+
+  private loadDir = (dir: string) => {
+    fs.readdir(dir, (err, files) => {
+      if (!err) {
+        this.setState({ files, prevDir: this.state.currentDir, currentDir: dir })
+      }
+    });
+  }
+
   render() {
-    return <h1 onClick={this.btnClick}>Hello from {this.props.compiler} and {this.props.framework}! Click counter is {this.state.counter}</h1>;
+    return <h1>
+      Hello from {this.props.compiler} and {this.props.framework}! Click counter is {this.state.counter}
+      <ul>
+        {this.state.prevDir && <li onClick={this.loadPrevDir}>..</li>}
+        {this.state.files.map(file => <li key={file} onClick={() => this.loadDir(`${this.state.currentDir}/${file}`)}>{file}</li>)}
+      </ul>
+    </h1>;
   };
 }
